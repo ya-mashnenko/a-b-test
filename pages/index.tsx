f124f86619ac45ee470565da7d7a51fa9438969d
Yaroslava Mashnenko <ya.mashnenko@gmail.com>
2022-09-25 21:29:59 +0200

    feat: add ga script

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,19 +3,29 @@ import type { NextPage } from "next";
 import { useState } from "react";
 import { Button } from "../components/Button/Button";
 import { Modal } from "../components/Modal/Modal";
+import Script from "next/script";
 
 const Home: NextPage = () => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   return (
-    <Flex w="100%" h="100%" id="around-button" justify="center" align="center">
-      <Button onClick={() => setIsModalOpen(true)} />
-      <Modal
-        isOpen={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
-        content="modal content"
-        title="title"
-      />
-    </Flex>
+    <>
+      <Script src="https://www.googleoptimize.com/optimize.js?id=4705124788" />
+      <Flex
+        w="100%"
+        h="100%"
+        id="around-button"
+        justify="center"
+        align="center"
+      >
+        <Button onClick={() => setIsModalOpen(true)} />
+        <Modal
+          isOpen={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+          content="modal content"
+          title="title"
+        />
+      </Flex>
+    </>
   );
 };
 
